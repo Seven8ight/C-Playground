cd7a3d581c6177e602f494dfadefce50cfbbc0fb
Muchiri <llwmuchiri@gmail.com>
2026-04-21 19:04:18 +0300

    Texture - exercise 2

--- Systems/Practice/Graphics Programming/OpenGL/Exercises - 3/GLSL/Frag1.glsl
+++ Systems/Practice/Graphics Programming/OpenGL/Exercises - 3/GLSL/Frag1.glsl
@@ -10,5 +10,5 @@ uniform sampler2D ourTexture2;
 
 void main(){
     // FragColor = texture(ourTexture,texCoords1) * vec4(mixColors,1.0);
-    FragColor = mix(texture(ourTexture,texCoords1),texture(ourTexture2,-texCoords2),0.75);
+    FragColor = mix(texture(ourTexture,texCoords1),texture(ourTexture2,texCoords2),0.75);
 }
